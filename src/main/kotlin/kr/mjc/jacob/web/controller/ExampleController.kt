dca0ff28f1a54f3dbd873100e07e354c66c0126f
w0nse0k <w0nse0k@naver.com>
2024-05-28 10:37:01 +0900

    Update ExampleController.kt

--- src/main/kotlin/kr/mjc/jacob/web/controller/ExampleController.kt
+++ src/main/kotlin/kr/mjc/jacob/web/controller/ExampleController.kt
@@ -6,6 +6,7 @@ import kr.mjc.jacob.web.repository.User
 import kr.mjc.jacob.web.repository.UserRepository
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.*
+import org.springframework.ui.Model
 import java.time.LocalDateTime
 import org.springframework.web.servlet.ModelAndView
 
